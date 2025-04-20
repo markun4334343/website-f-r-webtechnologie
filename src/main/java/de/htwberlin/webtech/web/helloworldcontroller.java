05b5dc1410b3ff2e75d557d98e1163da1249a01e
markun4334343 <rakun.mark@gmail.com>
2025-04-20 21:47:25 +0200

    init

---
+++ src/main/java/de/htwberlin/webtech/web/helloworldcontroller.java
@@ -0,0 +1,21 @@
+package de.htwberlin.webtech.web;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+public class helloworldcontroller {
+
+    @GetMapping(path ="/")
+    public ModelAndView showHelloWorldPage() {
+        return new ModelAndView("helloworld");
+    }
+
+
+
+
+
+}
